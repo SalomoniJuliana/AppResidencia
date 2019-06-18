1462a3e1da1cdeb79ae191d7ea709a3e5a30018a
SalomoniJuliana <salomoni.juliana@gmail.com>
2019-06-18 00:35:52 -0300

    commit de alteraçao para voltar, recarregar e tempo de espera

--- app/src/main/java/com/example/desafio/MainActivity.java
+++ app/src/main/java/com/example/desafio/MainActivity.java
@@ -5,10 +5,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
